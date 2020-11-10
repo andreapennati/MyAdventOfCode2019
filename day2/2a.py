851851079cb691b3d10ab45f020e317f9057c6da
Andrea Pennati <49478315+andreapennati@users.noreply.github.com>
2020-11-10 18:34:55 +0100

    Create .gitignore

--- day2/2a.py
+++ day2/2a.py
@@ -1,13 +1,23 @@
 import sys
 
+def add_bucket(bucket:list ,x:int ,y:int ,z:int):
+    bucket[z] = bucket[x] + bucket[y]
+
+def mul_bucket(bucket:list ,x:int ,y:int ,z:int):
+    bucket[z] = bucket[x] * bucket[y]
+
+dispatcher = {
+    1: add_bucket,
+    2: mul_bucket
+}
+
 bucket = [int(i) for i in open(sys.argv[1], 'r').read().split(',')]
 bucket[1] = 12
 bucket[2] = 2
 for i in range(0, len(bucket), 4):
     op, x, y, z = bucket[i:i+4]
-    if op == 1:
-        bucket[z] = bucket[x] + bucket[y]
-    elif op == 2:
-        bucket[z] = bucket[x] * bucket[y]
-    else: break
+    if op in dispatcher:
+        dispatcher[op](bucket,x,y,z)
+    else:
+        break
 print(bucket)
--- day2/2b.py
+++ day2/2b.py
@@ -2,16 +2,25 @@ import sys
 
 bucket = [int(i) for i in open(sys.argv[1], 'r').read().split(',')]
 
+def add_bucket(bucket:list ,x:int ,y:int ,z:int):
+    bucket[z] = bucket[x] + bucket[y]
+
+def mul_bucket(bucket:list ,x:int ,y:int ,z:int):
+    bucket[z] = bucket[x] * bucket[y]
+
+dispatcher = {
+    1: add_bucket,
+    2: mul_bucket
+}
+
 def calculate(bucket, s, t):
     bucket[1] = s
     bucket[2] = t
     i = 0
     for i in range(0, len(bucket), 4):
         op, x, y, z = bucket[i:i+4]
-        if op == 1:
-            bucket[z] = bucket[x] + bucket[y]
-        elif op == 2:
-            bucket[z] = bucket[x] * bucket[y]
+        if op in dispatcher:
+            dispatcher[op](bucket,x,y,z)
         else: break
     return bucket[0]
 
