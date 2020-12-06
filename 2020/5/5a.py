2d2b9dcc7994e822cbe4c2149192679c113fd856
Andrea Pennati <and.pennati@gmail.com>
2020-12-06 15:19:33 +0100

    day5 solutions

---
+++ 2020/5/5a.py
@@ -0,0 +1,20 @@
+import sys
+from math import floor, ceil
+
+def getRow(rules, start, end):
+    if end - start == 1 and (rules[0] == 'F' or rules[0] == 'L'):
+        return start
+    elif end - start == 1 and (rules[0] == 'B' or rules[0] == 'R'):
+        return end
+
+    if rules[0] == 'F' or rules[0] == 'L':
+        return getRow(rules[1:], start, floor((start + end) / 2))
+    else:
+        return getRow(rules[1:], ceil((start + end) / 2), end)
+
+result = []
+with open(sys.argv[1], 'r') as f:
+    for line in f.readlines():
+        cmd = line.strip('\n')
+        result.append(getRow(cmd[:7], 0, 127) * 8 + getRow(cmd[-3:], 0, 7))
+print(max(result))
